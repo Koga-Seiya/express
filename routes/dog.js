2280c5469e8d67e8c53b680a1019650075dfd882
古賀誠弥 <seiya01311224@gmail.com>
2023-12-05 13:26:14 +0900

    add dog api

--- routes/dog.js
+++ routes/dog.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const request = require('request');
 router.get('/', async (req, res) => {
-request('"https://images.dog.ceo/breeds/terrier-yorkshire/n02094433_2125.jpg"', function (error, response, 
+request('https://images.dog.ceo/breeds/terrier-yorkshire/n02094433_2125.jpg', function (error, response, 
 body) {
 if (!error && response.statusCode == 200) {
 const data = JSON.parse(body);
