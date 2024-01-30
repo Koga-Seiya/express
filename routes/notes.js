bef52f3c59be5877eb6b7a01041349b29789e8fa
古賀誠弥 <seiya01311224@gmail.com>
2024-01-30 16:00:11 +0900

    add env

--- routes/notes.js
+++ routes/notes.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
-const cors = require('cors');//corsミドルウェアを追加
+const cors = require('cors'); // corsミドルウェアを追加
 require('dotenv').config();
+
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-<<<<<<< HEAD
 const uri = process.env.MONGODB_URI;
-=======
-const uri = "******";
->>>>>>> eeb4b193510a9c0f105d30590284551168539532
 const client = new MongoClient(uri);
+
+// corsミドルウェアを使用
+router.use(cors());
 
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
@@ -24,5 +24,4 @@ const note = await notes.findOne(query);
 res.json(note);
 })
 
-module.exports = router;
 module.exports = router;
